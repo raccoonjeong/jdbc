1776d98d7ce8efae352844eadbab3cefb2528e39
raccoonjeong <rana3782@gmail.com>
2024-01-01 07:24:54 +0900

    데이터 접근 예외 직접 만들기

---
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV4Test.java
@@ -0,0 +1,111 @@
+package hello.jdbc.repository;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.service.MemberServiceV3_3;
+import hello.jdbc.service.MemberServiceV4;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+/**
+ * 예외 누수 문제 해결
+ * SQL Exception 제거
+ *
+ * MemberRepository 인터페이스 의존
+ */
+@Slf4j
+@SpringBootTest
+class MemberRepositoryV4Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+    @Autowired
+    private MemberRepository repository;
+    @Autowired
+    private MemberServiceV4 service;
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        DataSource dataSource() {
+            return new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        }
+
+        @Bean
+        PlatformTransactionManager transactionManager() {
+            return new DataSourceTransactionManager(dataSource());
+        }
+
+        @Bean
+        MemberRepository memberRepository() {
+            return new MemberRepositoryV4_1(dataSource());
+        }
+
+        @Bean
+        MemberServiceV4 memberServiceV4() {
+            return new MemberServiceV4(memberRepository());
+        }
+    }
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer() {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        repository.save(memberA);
+        repository.save(memberB);
+
+        // when
+        service.accountTransfer(memberA.getMemberId(), memberB.getMemberId(), 2000);
+        // then
+        Member findMemberA = repository.findById(memberA.getMemberId());
+        Member findMemberB = repository.findById(memberB.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(8000);
+        assertThat(findMemberB.getMoney()).isEqualTo(12000);
+    }
+    @Test
+    @DisplayName("이체중 예외 발생")
+    void accountTransferEx() {
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberEx = new Member(MEMBER_EX, 10000);
+        repository.save(memberA);
+        repository.save(memberEx);
+
+        // when
+        assertThatThrownBy(() -> service.accountTransfer(memberA.getMemberId(), memberEx.getMemberId(), 2000))
+                .isInstanceOf(IllegalStateException.class);
+
+        // then
+        Member findMemberA = repository.findById(memberA.getMemberId());
+        Member findMemberEx = repository.findById(memberEx.getMemberId());
+
+        assertThat(findMemberA.getMoney()).isEqualTo(10000);
+        assertThat(findMemberEx.getMoney()).isEqualTo(10000);
+    }
+
+    @AfterEach
+    void after() {
+        repository.delete(MEMBER_A);
+        repository.delete(MEMBER_B);
+        repository.delete(MEMBER_EX);
+    }
+
+}
